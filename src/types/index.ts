89551a254cc41b1c49019b7710bbd76f360aedb8
Aileeeeee <134634541+Aileeeeee@users.noreply.github.com>
2026-06-11 12:03:43 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -38,6 +38,7 @@ export interface LoginPayload {
 export interface SignupPayload {
   first_name:      string;
   last_name:       string;
+  username:        string;   
   email:           string;
   password:        string;
   organisation_id: number;
